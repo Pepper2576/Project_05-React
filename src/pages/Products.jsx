2c25d9018ddd4594e698f1888b65bf0aa71f85c7
Chris Pepper <pepper2576@gmail.com>
2024-11-22 15:56:37 +0000

    Tailwind and Bootstap added to project: tested and working

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ProductCard from '../components/ProductCard';
 import { DATA } from '../data/data';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Products = () => {
   return (
@@ -15,6 +16,12 @@ const Products = () => {
           />
         );
       })}
+      <h1 className='tw-text-3xl tw-font-bold tw-underline tw-text-blue-500'>
+        Hello world!
+      </h1>
+      <button type='button' class='btn btn-dark '>
+        Primary
+      </button>
     </div>
   );
 };
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'tw-',
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/pages/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
