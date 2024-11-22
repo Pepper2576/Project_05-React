83b19215ee40b9ab4cec1ac54f7ea6a83f21aa51
Chris Pepper <pepper2576@gmail.com>
2024-11-22 16:14:19 +0000

    README.md file content deleted

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ProductCard from '../components/ProductCard';
 import { DATA } from '../data/data';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Products = () => {
   return (
@@ -16,12 +15,6 @@ const Products = () => {
           />
         );
       })}
-      <h1 className='tw-text-3xl tw-font-bold tw-underline tw-text-blue-500'>
-        Hello world!
-      </h1>
-      <button type='button' class='btn btn-dark '>
-        Primary
-      </button>
     </div>
   );
 };
