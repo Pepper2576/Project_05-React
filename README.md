2b4f2edf9cbbad2d227d0cb1b2924d8890b78225
Chris Pepper <pepper2576@gmail.com>
2024-11-24 15:48:08 +0000

    Navbar created with bootstrap

---
+++ README.md
@@ -0,0 +1,4 @@
+to use bootstrap in project ensure you add:
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+to the page / component
--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Products from './pages/Products';
+import Navbar from './components/Navbar';
 
 function App() {
-  return <Products className='test' />;
+  return (
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Products />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
