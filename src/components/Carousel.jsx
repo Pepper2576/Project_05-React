ac771f00d813ec5f694226fdfe1895af9bdf042b
Chris Pepper <pepper2576@gmail.com>
2024-11-25 15:03:20 +0000

    Carousel added to homepage and styling begun

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { CAROUSELDATA } from '../data/carouselData';
+
+export default function Carousel() {
+  return (
+    <div>
+      <div
+        id='carouselExampleSlidesOnly'
+        class='carousel slide'
+        data-bs-ride='carousel'
+      >
+        <div class='carousel-inner'>
+          {CAROUSELDATA.map((item) => {
+            return (
+              <div class='carousel-item active' key={item.id}>
+                <img src={item.img} class='d-block w-100' alt={item.title} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
+import Carousel from '../components/Carousel';
+import '../styles/home-page.css';
 
 export default function Home() {
-  return <div>Home page</div>;
+  return (
+    <div className='home-body '>
+      Home page
+      <div className='tw-col-start-3 tw-col-end-5 tw-row-start-1 tw-row-end-3 tw-m-5'>
+        <Carousel />
+      </div>
+    </div>
+  );
 }
